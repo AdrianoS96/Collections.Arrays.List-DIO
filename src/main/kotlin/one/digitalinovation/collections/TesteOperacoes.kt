ef5809d0cea6e77e5272b346c3df89c1e79f6f18
Adriano S96 <adrianodes96@gmail.com>
2021-03-27 10:21:52 -0300

    Teste de operações .max .min .average .filter .count .find .any

--- src/main/kotlin/one/digitalinovation/collections/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinovation/collections/TesteOperacoes.kt
@@ -17,4 +17,19 @@ fun main() {
 
     val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
     salariosMaiorQue2500.forEach{ println(it)}
+
+    println("-------------")
+
+    println(salarios.count{it in 2000.0 .. 5000.0}) //contar a quantidade de elemendos dentro do range
+
+    println("-------------")
+
+    println(salarios.find{it == 4839.2}) // conta a quantidade de elementos com valor igual ao definido
+    println(salarios.find{it == 2000.0})
+
+    println("-------------")
+
+    println(salarios.any{it == 4839.2})//procura se a expressão é verdadeira
+    println(salarios.any{it == 2000.0})
+
 }
